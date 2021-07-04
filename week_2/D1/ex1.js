e9d386960c424f3f4f02d81466376347a6c99644
ralfrpala <77062574+ralfrpala@users.noreply.github.com>
2021-07-04 12:01:04 +0300

    Create ex1.js

---
+++ week_2/D1/ex1.js
@@ -0,0 +1,5 @@
+let addressNumber = 83 //number
+let addressStreet = "Rambam" //string
+let country = "Israel" //string
+let globalAdress = "I live in "+ addressStreet +" "+ addressNumber +", in " + country
+console.log(globalAdress)
