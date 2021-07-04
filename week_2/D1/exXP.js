bee702c0056ca8c22511e5868ca02e8a168e9f34
ralfrpala <77062574+ralfrpala@users.noreply.github.com>
2021-07-04 13:09:51 +0300

    Create exXP.js

---
+++ week_2/D1/exXP.js
@@ -0,0 +1,13 @@
+    5 + "34" //guess:39 answer:534
+    5 - "4" //guess:?? answer:1
+    10 % 5 //guess:0 answer:0
+    5 % 10 //guess:?? answer:5
+    "Java" + "Script" //guess:JavaScript answer:JavaScript
+    " " + " " //guess:"  " answer:"  "
+    " " + 0 //guess: 0 answer:0
+    true + true //guess:true answer: 2
+    true + false //guess:false answer: 1
+    false + true //guess:false answer: 1
+    false - true //guess:?? answer:-1
+    3 - 4 //guess: -1 answer: -1
+    "Bob" - "bill" //guess:NaN answer: NaN
