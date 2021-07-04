b6b690ecbb514a1346117cb74ad20e42eee0187d
ralfrpala <77062574+ralfrpala@users.noreply.github.com>
2021-07-04 12:07:17 +0300

    Create ex2.js

---
+++ week_2/D1/ex2.js
@@ -0,0 +1,5 @@
+let birth = 2007 //birth year
+let future = 2035 //random year in the future
+let futureAge = 2035-2007 //age in the future
+
+console.log("I will be "+ futureAge + " years old in "+ future)
