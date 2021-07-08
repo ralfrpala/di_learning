bced91138e212cc32e05ec1347485d5a55f319be
ralfrpala <77062574+ralfrpala@users.noreply.github.com>
2021-07-08 13:22:30 +0300

    Create DAILY.js

---
+++ week_2/D5/DAILY.js
@@ -0,0 +1,27 @@
+//99
+//take 1 down
+//98
+//2 down
+//96
+//3 down
+//93
+let beers = 99
+function beer() {
+	let starting = prompt("enter a number")
+	for (let i=starting; i<99; i++){
+		console.log(beers+" bottles of beer on the wall, "+beers+" bottles of beer")
+		beers= beers-i
+		if (beers<0) {
+			console.log("got to go to the market to get more beer bottles of beers for the wall")
+			return
+		}
+		console.log("take "+i+" out")
+		if (i==1) {
+			console.log("pass it around")
+		}else{console.log("pass them around")}
+		console.log(beers+" bottles of beer on the wall!")
+		console.log(" ")
+
+	}
+}
+beer()
